291bddfbc35ef0b824ac69a5e2bd117dbc1cae13
melhassouni <mohamedali.elhsn@gmail.com>
2025-08-22 21:53:28 +0100

    new changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -527,6 +527,7 @@ Forget about spam, advertising mailings, hacking and attacking robots. Keep your
             <p class="text-start" style="margin-bottom: 4px;font-size: 16px;">
                 Made with lots of 🥨 in Germany
                 <span class="float-end">
+                    <a href="/" style="color: inherit;">Email Generator</a>&nbsp;&nbsp;
                     <a href="/privacy-policy" style="color: inherit;">Privacy</a>&nbsp;&nbsp;
                     <a href="/terms" style="color: inherit;">Terms</a>&nbsp;&nbsp;
                     <a href="https://github.com/berrysauce/justatemp/blob/main/LICENSE" target="_blank" style="color: inherit;">License</a>&nbsp;&nbsp;
