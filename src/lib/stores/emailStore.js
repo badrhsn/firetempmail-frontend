94c3da47ec5738b9ff1e52edbf587143198eaa9d
melhassouni <mohamedali.elhsn@gmail.com>
2025-08-22 17:05:35 +0100

    new changes

--- src/lib/stores/emailStore.js
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const receivingEmail = writable(null);
-export const emails = writable([]);
-export const stats = writable({});
-export const selectedEmail = writable(null);
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import '../app.css';
-</script>
-
-<slot />
-
-<!-- This will make the ToastContainer available on all pages -->
-<ToastContainer />
