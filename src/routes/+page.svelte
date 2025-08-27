e2ac4bf339ca612ee11964fad01aaf5edafcdca7
melhassouni <mohamedali.elhsn@gmail.com>
2025-08-27 12:28:08 +0100

    new changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -409,7 +409,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
                             <path d="M19 7L18.1327 19.1425C18.0579 20.1891 17.187 21 16.1378 21H7.86224C6.81296 21 5.94208 20.1891 5.86732 19.1425L5 7M10 11V17M14 11V17M15 7V4C15 3.44772 14.5523 3 14 3H10C9.44772 3 9 3.44772 9 4V7M4 7H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                        Delete Email
+                        Delete
                     </button>
                 </div>
             </div>
@@ -1180,7 +1180,6 @@ A <strong>disposable email address</strong> is a free <strong>temporary email se
     }
     
     .btn {
-        padding: 10px 20px;
         border-radius: 8px;
         cursor: pointer;
         font-weight: 500;
@@ -1288,7 +1287,7 @@ A <strong>disposable email address</strong> is a free <strong>temporary email se
     }
     
     .email-action-buttons {
-        display: flex;
+        display: -webkit-inline-box;
         gap: 12px;
         justify-content: center;
         flex-wrap: wrap;
@@ -1671,7 +1670,7 @@ A <strong>disposable email address</strong> is a free <strong>temporary email se
         
         
         .btn {
-            min-width: 100%;
+            min-width: 50%;
             justify-content: center;
         }
         
