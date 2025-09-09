38697692427ce6df5ada99b1d3a60fed00f899ea
melhassouni <mohamedali.elhsn@gmail.com>
2025-09-09 13:04:46 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import purgecss from '@fullhuman/postcss-purgecss';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		postcss: {
+			plugins: [
+				purgecss({
+					content: ['./src/**/*.svelte', './src/app.html'],
+					safelist: {
+						standard: ['html', 'body', /data-bs-theme/]
+					}
+				})
+			]
+		}
+	}
 });
