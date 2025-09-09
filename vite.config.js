7b0b1ab5a9905e5600c0468ec9ac95d4795b41f5
melhassouni <mohamedali.elhsn@gmail.com>
2025-09-09 12:38:06 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		visualizer({
-			filename: 'dist/stats.html',
-			open: true,
-		}),
-	],
-	build: {
-		sourcemap: true,
-		rollupOptions: {
-			output: {
-				manualChunks: (id) => {
-					if (id.includes('node_modules')) {
-						return 'vendor';
-					}
-				},
-			},
-		},
-	},
+	plugins: [sveltekit()]
 });
