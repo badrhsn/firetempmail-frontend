ffbfa329a29adbe67919ee35feba668bdc2448cb
melhassouni <mohamedali.elhsn@gmail.com>
2025-09-12 13:06:17 +0100

    new changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-cloudflare';
-
+import adapter from '@sveltejs/adapter-auto'; 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// Use the Cloudflare adapter
-		adapter: adapter(),
-
-		// Enable SSR to render HTML in the source
-		ssr: true,
-
-		// Optional: If deploying on a subdirectory or specific routes
-		// paths: {
-		//     base: '/frontend'
-		// }
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
