382a19f4adc78a5c015711ca4183c19ee5da15e0
Badr eddine El hassouni <badr@Badrs-MacBook-Air.local>
2025-12-01 23:22:13 +0100

    new update

--- src/routes/ads.txt/+server.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-    return new Response(null, {
-        status: 301,
-        headers: {
-            'Location': 'https://srv.adstxtmanager.com/19390/firetempmail.com'
-        }
-    });
-}
