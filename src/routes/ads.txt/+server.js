ffa720eef67bdf83bad80c3c57d6ee27b7113b73
badrhsn <badr.hsn96@gmail.com>
2026-07-29 14:38:11 +0100

    edits by bad

--- src/routes/ads.txt/+server.js
+++ src/routes/ads.txt/+server.js
@@ -1,11 +1,4 @@
-import adsTxt from '$lib/server/ads.txt?raw';
-
 /** @type {import('./$types').RequestHandler} */
 export function GET() {
-    return new Response(adsTxt, {
-        headers: {
-            'Content-Type': 'text/plain; charset=utf-8',
-            'Cache-Control': 'public, max-age=300, must-revalidate'
-        }
-    });
+    return Response.redirect('https://srv.adstxtmanager.com/19390/firetempmail.com', 301);
 }
