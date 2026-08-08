0bb415fc0dc04b6d7f9bb1293215286361b0d44a
badrhsn <badr.hsn96@gmail.com>
2026-08-08 13:50:53 +0100

    edits by bad

--- src/routes/ads.txt/+server.js
+++
@@ -1,4 +0,0 @@
-/** @type {import('./$types').RequestHandler} */
-export function GET() {
-    return Response.redirect('https://adstxt.journeymv.com/sites/a97ac20f-4fe3-4f21-98bf-c4b6d0ccf662/ads.txt', 301);
-}
